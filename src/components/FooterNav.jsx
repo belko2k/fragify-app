811b6f9f2551470d71cbe4862373af94140622e8
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-07-19 14:51:42 +0200

    Footer nav

---
+++ src/components/FooterNav.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import styles from '../styles/FooterNav.module.css';
+
+const FooterNav = () => {
+  return (
+    <nav className={styles['footer-nav']}>
+      <ul aria-label="Footer Navigation" role="list">
+        <div className="left-list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About Us</Link>
+          </li>
+          <li>
+            <Link to="/">Careeres</Link>
+          </li>
+        </div>
+        <div className="right-list">
+          <li>
+            <Link to="/">Community</Link>
+          </li>
+          <li>
+            <Link to="/">Terms & Conditions</Link>
+          </li>
+          <li>
+            <Link to="/">Privacy Policy</Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default FooterNav;
