f0839847e87ba485e2d392cbc98f168ce69f5776
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-07-20 13:46:11 +0200

    women const

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,15 +15,48 @@ export const men = [
         prices: [
             {
                 bottle_size: "50ml",
-                price: 80.5
+                price: 82.5
             },
             {
                 bottle_size: "100ml",
-                price: 110.0
+                price: 114.0
             },
             {
                 bottle_size: "150ml",
-                price: 150.0
+                price: 128.0
+            }
+        ]
+    }
+];
+
+export const women = [
+    {
+        id: 'la_vie_est_belle',
+        name: 'La Vie Est Belle',
+        brand: 'Lancôme',
+        image: dhi,
+        notes: {
+            top: [ "Black Currant", "Pear" ],
+            middle: [ "Iris", "Jasmine", "Orange Blossom" ],
+            base: [ "Praline", "Vanilla", "Patchouli", "Tonka Bean" ]
+        },
+        perfumers: [ "Anne Flipo", "Dominique Ropion", "Olivier Polge" ],
+        prices: [
+            {
+                bottle_size: "30ml",
+                price: 58.9
+            },
+            {
+                bottle_size: "50ml",
+                price: 78.9
+            },
+            {
+                bottle_size: "100ml",
+                price: 98.0
+            },
+            {
+                bottle_size: "150ml",
+                price: 158.5
             }
         ]
     }
