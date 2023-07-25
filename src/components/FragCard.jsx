192938d2d29edf0948b0a718bd8993933b8f43a5
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-07-25 17:26:29 +0200

    card proptypes

--- src/components/FragCard.jsx
+++ src/components/FragCard.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 import styles from '../styles/FragCard.module.css';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-const FragCard = ({ id, image, brand, name }) => {
+const FragCard = ({ id, image, brand, name, prices }) => {
   return (
     <Link to={id} key={id}>
       <div className={styles['card']}>
@@ -10,10 +10,26 @@ const FragCard = ({ id, image, brand, name }) => {
         <div className={styles['details']}>
           <p className={styles.brand}>{brand}</p>
           <p className={styles.name}>{name}</p>
+          <div className={styles.price}>
+            {prices[0].price.toFixed(2).replace('.', ',')}€
+          </div>
         </div>
       </div>
     </Link>
   );
+};
+
+FragCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  prices: PropTypes.arrayOf(
+    PropTypes.shape({
+      bottle_size: PropTypes.string,
+      price: PropTypes.number,
+    }).isRequired
+  ).isRequired,
 };
 
 export default FragCard;
