bf872880295b401835c064a2a3a9b03b2ffef282
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-08-14 15:17:51 +0200

    nested li tags (perfumers)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import Navbar from './Navbar';
 import styles from '../styles/Header.module.css';
 import logo from '../assets/logo-black.png';
-import { FaCartShopping, FaMagnifyingGlass } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -10,9 +9,6 @@ const Header = () => {
       <Link to="/">
         <img className={styles.logo} src={logo} alt="logo" />
       </Link>
-
-      <FaMagnifyingGlass />
-      <FaCartShopping />
       <Navbar />
     </header>
   );
